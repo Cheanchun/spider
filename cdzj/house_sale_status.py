c94b668cb18b08fc728255fa6502abba91617fba
chean <32571056@qq.com>
2021-01-26 16:19:45 +0800

    selenium 通用下载 配置

--- cdzj/house_sale_status.py
+++ cdzj/house_sale_status.py
@@ -5,7 +5,6 @@
 @Desc:
 @Todo
 """
-import uuid
 
 import pyamf
 import requests
@@ -20,34 +19,47 @@ headers = {
     "Accept-Encoding": "gzip, deflate, br",
     "Origin": "https://zw.cdzj.chengdu.gov.cn",
     "Connection": "keep-alive",
-    "Cookie": "JSESSIONID=FF018C929B24DB26636D2E65195BD3DF; FntCookie=TraditionalOrSimplized=0; ASP.NET_SessionId=pygkmqjupi54xr55hehrhm55",
+    "Cookie": "JSESSIONID=F971539AF85A8CCB87B2616BBE39E4E1; FntCookie=TraditionalOrSimplized=0; ASP.NET_SessionId=pygkmqjupi54xr55hehrhm55",
     "Referer": "https://zw.cdzj.chengdu.gov.cn/DE-SMServerFx/FundateClient.swf?t=1&param=dfKr9U7lple67abd5wFvyI52vkx2j%2FvdwKNC2r1C2Tk4%2BpBjAwp%2BqT%2BNJmFaGXgbtQyK5%2FGlb%2FRJcJCg2ZIyJoIlxDrrlrur%2BsPFzKImAbhQoEmW9DnHnWsm0HvtCY4O3xhF0a%2BN5a%2F28vYK8DU%2FA%2FZbcL%2B3PE8C26punMzcYnc%3D",
     "Content-type": "application/x-amf",
     "Content-length": "519",
 
 }
 
-
-class HqPara:
-    def __init__(self):
-        self.source = None
+msg = messaging.RemotingMessage()
 
 
-msg = messaging.RemotingMessage(messageId=str(uuid.uuid1()).upper(),
-                                clientId=str(uuid.uuid1()).upper(),
-                                operation='getHqSearchData',
-                                destination='reportStatService',
-                                timeToLive=0,
-                                timestamp=0)
-
-
-def getRequestData(page_num, total_num):
+def getRequestData():
     # 请求体第一个参数是查询参数, 第二个是页数,第三个是控制每页显示的数量
-    msg.body = [HqPara(), str(page_num), str(total_num)]
-    msg.headers['DSEndpoint'] = None
-    msg.headers['DSId'] = str(uuid.uuid1()).upper()
+    msg.Content = [
+        {
+            "source": None,
+            "operation": 'ExecRPC',
+            "timestamp": 0,
+            "clientId": None,
+            "destination": "SMService",
+            "headers": {
+                "DSEndpoint": 'my-amf',
+                "DSId": "1D666BDD-0F18-30E5-7841-1C99D348FF23"
+            },
+            "messageId": "ED56AEE0-0722-5EF5-ABBF-3D9A716284EF",
+            "body": [
+                "HouseTableServiceImpl",
+                "getHouses",
+                [
+                    {
+                        "UNO": 1,
+                        "HNO": 7,
+                        "paramStr": "dfKr9U7lple67abd5wFvyI52vkx2j/vdwKNC2r1C2Tk4+pBjAwp+qT+NJmFaGXgbtQyK5/Glb/RJcJCg2ZIyJoIlxDrrlrur+sPFzKImAbhQoEmW9DnHnWsm0HvtCY4O3xhF0a+N5a/28vYK8DU/A/ZbcL+3PE8C26punMzcYnc=",
+                    }
+                ],
 
-    req = remoting.Request('null', body=(msg,))
+            ],
+            "timeToLive": 0
+        }
+    ]
+
+    req = remoting.Request('null',body=msg)
     env = remoting.Envelope(amfVersion=pyamf.AMF3)
     env.bodies = [('/1', req)]
     data = bytes(remoting.encode(env).read())
@@ -59,5 +71,16 @@ def getResponse(data):
     # 这个url一定是amf结尾的哦
     # url = 'http://jgsb.agri.cn/controller?SERVICE_ID=REGISTRY_JCSJ_MRHQ_SHOW_SERVICE&recordperpage=15&newsearch=true&login_result_sign=nologin'
     url = 'https://zw.cdzj.chengdu.gov.cn/DE-SMServerFx/messagebroker/amf'
-    response = requests.post(url, data, headers={'Content-Type': 'application/x-amf'})
+    response = requests.post(url, data, headers=headers, verify=False)
     return response.content
+
+
+def getContent(response):
+    parse_info = remoting.decode(response)
+
+    # total_num = parse_info.bodies[0][1].body.body[3]
+    # info = parse_info.bodies[0][1].body.body[0]
+    return parse_info
+
+
+print(getContent(getResponse(getRequestData())))
