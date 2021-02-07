f072f4226b9889fb94058f83d2e1bbc46d5ba72d
chean <32571056@qq.com>
2021-02-07 18:06:41 +0800

    cps

--- centrepointstores/temp.py
+++ centrepointstores/temp.py
@@ -7,6 +7,7 @@ https://lm8x36l8la-dsn.algolia.net/1/indexes/*/queries?X-Algolia-API-Key=7a625e3
 https://lm8x36l8la-dsn.algolia.net/1/indexes/*/queries?X-Algolia-API-Key=7a625e3a548ccdd0393b5565896c5d89&X-Algolia-Application-Id=LM8X36L8LA&X-Algolia-Agent=Algolia%20for%20vanilla%20JavaScript%202.9.7
 Request Method: POST
 """
+import datetime
 import json
 import os
 import re
@@ -231,8 +232,8 @@ def get_main_category():
 FIRST_CATE = 'Woman-Clothing'
 
 
-def data_parse(resp: requests.Response, sec_cate, third_cate):
-    source_data = resp.json().get('results')[0].get('hits')
+def data_parse(resp, sec_cate='', third_cate=''):
+    source_data = resp.get('results')[0].get('hits')
     for item in source_data:
         index_product = {}
         index_product['color'] = 'Multi'
@@ -240,16 +241,18 @@ def data_parse(resp: requests.Response, sec_cate, third_cate):
         if temp_id.isdigit():
             index_product['product_id'] = temp_id
         else:
-            res = re.search(r'[a-zA-Z]+', temp_id).group()
-            index_product['color'] = res
-            index_product['product_id'] = temp_id.replace(res, '')
+            res = re.findall(r'[a-zA-Z]+', temp_id)
+            _id = max(res, key=len)
+            index_product['color'] = max(res, key=len)
+            index_product['product_id'] = temp_id.replace(_id, '')
         index_product['price_origin(AED)'] = item.get('wasPrice')
         index_product['price_sale(AED)'] = item.get('price')
         index_product['pic_url'] = item.get('333WX493H')
         p_url = item.get('url')
         for key, value in p_url.items():
             url = p_url[key].get('en')
-            index_product['product_url'] = urljoin(resp.url, url)
+            index_product['product_url'] = urljoin('https://www.centrepointstores.com/', url)
+            sec_cate, third_cate = key.split('-')[-2], key.split('-')[-1]
             break
         index_product['first_category'] = FIRST_CATE
         index_product['second_category'] = sec_cate
@@ -262,63 +265,37 @@ def data_parse(resp: requests.Response, sec_cate, third_cate):
         save_data(index_product)
         if item.get('sibiling'):
             for sibiling_item in item.get('sibiling'):
+                temp_id = sibiling_item.get('code')
+                if temp_id.isdigit():
+                    index_product['product_id'] = temp_id
+                else:
+                    res = re.findall(r'[a-zA-Z]+', temp_id)
+                    _id = max(res, key=len)
+                    index_product['color'] = max(res, key=len)
+                    index_product['product_id'] = temp_id.replace(_id, '')
                 sibiling_data = deepcopy(index_product)
-                sibiling_data['product_id'] = sibiling_item.get('code')
                 sibiling_data['color'] = sibiling_item.get('color')
                 save_data(index_product)
-    return resp.json().get('results')[0].get('nbPages')
-
-
-def item(p_id='5528116'):
-    p_url = "https://www.centrepointstores.com/ae/en/c/cpwomen-clothing-bottoms-jeans"
-    print(p_url)
-    h = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "cache-control": "no-cache",
-        "cookie": "__cfduid=d9760f35c96bcbf58ef2090ab3760c6fa1612659883; JSESSIONID=31BF75A22F2D853CD6A9A3FE14E3A71F; _traceId=76de6004-68e0-11eb-aedd-a9f261e5020f-W; _lmgLan=en; lmg_uid=Ch/kMGAfPKy3rYBpasCDAg==; b2pi=!G4J4pUlGjKcIpRMAWbC7yci1H+z77ZnZNL4K6ZkmkMItooDlYOl7FyediiU+/CGZ5PfcsKFruRA/+Q==; __cfruid=d95e8a2f976a82a7964303a145a516579743655a-1612659884; _app_deviceId=user_16126600464967314; mt.v=2.2033306531.1612660048991; moe_uuid=331c6773-327d-4150-863f-b7409e8a5837; b1pi=!LmCvxjg8s+0HdRAAWbC7yci1H+z77cOMdnBIzr8FDLuaPWRB2xT5W7IUKvUZXgWuMabOl4G2rhxXMw==; alg_location=; _lmgfpc=""; mt.sc=%7B%22i%22%3A1612662783116%2C%22d%22%3A%5B%5D%7D; __cf_bm=88fc62969323b8484f8e80eb10e2aa43e7fcc0d2-1612663122-1800-AZLl3QRMHEyditrOYFdL8nIy7jEdWOzt7gCqzar3ZTHCTiyjhQBw7kgk4HeAbjhB3o7S8SLDcRko5F6zWF4rzK8=",
-        "pragma": "no-cache",
-        "referer": "https://www.centrepointstores.com/ae/en/c/cpwomen-footwear-sportsshoes",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
-    }
-    resp = requests.get(p_url, headers=h, proxies=proxy, verify=False)
-    print(resp.status_code)
+    return resp.get('results')[0].get('nbPages')
 
 
 def save_data(data):
     temp = deepcopy(data)
+    temp['date'] = str(datetime.datetime.now())
     col.insert(temp)
 
 
 def main():
-    for category, items in CATEGORY_DICT.items():
-        # time.sleep(10)
-        print(category)
-        for params in items:
+    file_d = './old_data'
+    for params in os.listdir(file_d):
+        for item in os.listdir(os.path.join(file_d, params)):
+            file_path = os.path.join(file_d, params, item)
             try:
-                third_cate = params.split('-')[-1]
-                file_dir = os.path.join(os.getcwd(), FIRST_CATE, category, third_cate)
-                # try:
-                #     os.makedirs(file_dir)
-                # except Exception:
-                #     pass
-                print('category:{}'.format(params))
-                resp = get_product(params, file_dir=file_dir)
-                t_page = data_parse(resp, category, third_cate)
-                for page in range(1, t_page):
-                    time.sleep(1)
-                    print('page:{}'.format(page))
-                    resp = get_product(category, page, file_dir=file_dir)
-                    data_parse(resp, category, third_cate)
+                with open(file_path, mode='r', encoding='u8') as fp:
+                    data_json = json.loads(fp.read())
+                    data_parse(data_json)
             except Exception as e:
-                print(e)
-                user_redis.sadd('clp_error', params)
+                print(e, file_path)
 
 
 if __name__ == '__main__':
