e2b81743f51a886a954400cfef3b60cfb0dd7222
cheanchun <32571056@qq.com>
2021-04-02 00:11:10 +0800

    1

--- simuwang/spider.py
+++ simuwang/spider.py
@@ -56,7 +56,7 @@ def get_css(url):
         "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
         "Cache-Control": "max-age=0",
         "Connection": "keep-alive",
-            "Cookie": "Hm_lvt_c3f6328a1a952e922e996c667234cdae=1617098359,1617261604; need_update_password=1; smppw_tz_auth=1; http_tK_cache=a71ab2beb738b91c113ee3491c0de05f9881b79b; cur_ck_time=1617262764; ck_request_key=jHhggNiAN2cq85XLsVHeRSHXSmFYf2Q5qjxeN69aBBY%3D; passport=1033896%09u3960723742121%09BQUFBQwAAQldUwQBWlBWDFJcAQEGCgkJAFZeXgYHAVY%3D9710d4c4e7; certification=1; qualified_investor=1; evaluation_result=2; focus-certification-pop=-1; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%221033896%22%2C%22first_id%22%3A%221033896%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22%24device_id%22%3A%221788292c1ad34e-018aa92c391987-5c173a1b-1049088-1788292c1ae527%22%7D; Hm_lpvt_c3f6328a1a952e922e996c667234cdae=1617277407",
+        "Cookie": "Hm_lvt_c3f6328a1a952e922e996c667234cdae=1617098359,1617261604; need_update_password=1; smppw_tz_auth=1; http_tK_cache=a71ab2beb738b91c113ee3491c0de05f9881b79b; cur_ck_time=1617262764; ck_request_key=jHhggNiAN2cq85XLsVHeRSHXSmFYf2Q5qjxeN69aBBY%3D; passport=1033896%09u3960723742121%09BQUFBQwAAQldUwQBWlBWDFJcAQEGCgkJAFZeXgYHAVY%3D9710d4c4e7; certification=1; qualified_investor=1; evaluation_result=2; focus-certification-pop=-1; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%221033896%22%2C%22first_id%22%3A%221033896%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22%24device_id%22%3A%221788292c1ad34e-018aa92c391987-5c173a1b-1049088-1788292c1ae527%22%7D; Hm_lpvt_c3f6328a1a952e922e996c667234cdae=1617277407",
         "Host": "dc.simuwang.com",
         "If-None-Match": 'W/"1d17e-io308fQP6cgs/rbs5h431zc5XTs"',
         "Sec-Fetch-Dest": "document",
@@ -73,19 +73,20 @@ def get_css(url):
 
 def convert_num(css_num, p_code):
     show_map = {'HF00005V8X': {
-        'b722d0': True,
-        'b7f624': True,
-        'b77ba4': False,
-        'b7ad05': False,
-        'b7d86a': True,
-        'b7f7af': True,
-        'b752c4': False
+        'e722d0': True,
+        'e7f624': True,
+        'e77ba4': False,
+        'e7ad05': False,
+        'e7d86a': True,
+        'e7f7af': True,
+        'e752c4': False
     },
         'HF0000122L': {
 
         }
     }
-    return show_map.get(css_num)
+    item_map = show_map.get(p_code)
+    return item_map.get(css_num)
 
 
 if __name__ == '__main__':
@@ -98,7 +99,7 @@ if __name__ == '__main__':
             p_code = p_url.rsplit('/')[-1].split('.')[0]
             print('|'.join([p_name, p_url, p_code]))
             content = fp.readline().strip()
-            res_data = data_request('HF00005V8X', 2).get('data')
+            res_data = data_request('HF00005V8X', 1).get('data')
             pager = res_data.get('pager')
             data_list = res_data.get('data')
             while data_list:
@@ -110,7 +111,7 @@ if __name__ == '__main__':
                     values = html.xpath('//text()')
                     s = ''
                     for c, v, in zip(h_class, values):
-                        if convert_num(c):
+                        if convert_num(c,p_code):
                             s += str(v)
                     total_pure_value = s
                     print(date, s)
